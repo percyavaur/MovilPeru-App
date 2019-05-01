f7811c34571869e02ff8331dd54bcf7c6630b9ba
percyavaur <percyavaur@hotmail.com>
2019-05-01 12:00:32 -0500

    update drawernavigator
    update drawernavigator

--- screens/TestScreen.js
+++ screens/TestScreen.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import {
   Image,
-  Platform,
-  ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
 } from 'react-native';
-import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
+import Icon from '@expo/vector-icons/Ionicons';
 
 export default class HomeScreen extends React.Component {
 
