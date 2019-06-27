4682e04a5262ded31c606a0e0b9f1b586cf7b119
percyavaur <percyavaur@hotmail.com>
2019-06-27 03:06:51 -0500

    update user trups
    update user trups

--- screens/userTrips/UserTrips.js
+++ screens/userTrips/UserTrips.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, ScrollView, Dimensions, Image , RefreshControl, FlatList} from 'react-native';
+import { StyleSheet, ScrollView, Dimensions, Image, RefreshControl, FlatList } from 'react-native';
 import { Card, CardItem, Body, Text, View } from "native-base";
 import { NavigationOptions2 } from "../../navigation/NavigationOptions";
 import { _GetAsyncStorage } from "./../../utils/asyncStorage/getAsyncStorage";
@@ -12,8 +12,8 @@ export default class UserTrips extends React.Component {
         return NavigationOptions2(navigation);
     };
 
-    state={
-        pasajes:[{}]
+    state = {
+        pasajes: [{}]
     }
 
     componentDidMount() {
@@ -36,10 +36,15 @@ export default class UserTrips extends React.Component {
                     this.setState({ pasajes: data.data });
                 });
     }
+    timeStampToDate(timestamp) {
+        var t = timestamp.split(/[- :]/);
+        var d = t[0]+"-"+(t[1] - 1)+"-"+t[2]
+        return d;
+    }
 
     render() {
 
-        const {pasajes} = this.state;
+        const { pasajes } = this.state;
 
         return (
             <View style={{ backgroundColor: "#f7fafc" }}>
@@ -52,6 +57,72 @@ export default class UserTrips extends React.Component {
                         extraData={this.state}
                         renderItem={({ item, index }) => (
                             <Card key={index}>
+                                <CardItem header bordered>
+
+                                    <Text>{item.fechaCompra ? this.timeStampToDate(item.fechaCompra) : null}</Text>
+                                </CardItem>
+                                <CardItem header bordered>
+                                    <Text>
+                                        Ida
+                                        </Text>
+                                </CardItem>
+                                <CardItem bordered>
+                                    <Body>
+                                        <View style={{ flex: 1, flexDirection: 'column' }}>
+                                            <Text style={[styles.text, { fontWeight: "bold" }]}>
+                                                Origen
+                                            </Text>
+                                            <Text style={[styles.text, { marginLeft: 10, marginVertical: 5 }]}>
+                                                {item.idaOrigen}
+                                            </Text>
+                                            <Text style={[styles.text, { fontWeight: "bold" }]}>
+                                                Destino
+                                            </Text>
+                                            <Text style={[styles.text, { marginLeft: 10, marginVertical: 5 }]}>
+                                                {item.idaDestino}
+                                            </Text>
+                                        </View>
+                                    </Body>
+                                </CardItem>
+                                {
+                                    item.vueltaOrigen ?
+                                        <CardItem header bordered>
+                                            <Text style={[styles.text]}>
+                                                Vuelta
+                                            </Text>
+                                        </CardItem>
+                                        : null
+                                }
+                                {item.vueltaDestino ?
+
+                                    <CardItem bordered>
+                                        <View style={{ flex: 1, flexDirection: 'column' }}>
+                                            <Text style={[styles.text, { fontWeight: "bold" }]}>
+                                                Origen
+                                        </Text>
+                                            <Text style={[styles.text, { marginLeft: 10, marginVertical: 5 }]}>
+                                                {item.vueltaOrigen}
+                                            </Text>
+                                            <Text style={[styles.text, { fontWeight: "bold" }]}>
+                                                Destino
+                                        </Text>
+                                            <Text style={[styles.text, { marginLeft: 10, marginVertical: 5 }]}>
+                                                {item.vueltaDestino}
+                                            </Text>
+                                        </View>
+                                    </CardItem>
+                                    : null
+                                }
+                                <CardItem header bordered>
+                                    <Text>
+                                        Pasajero
+                                    </Text>
+                                </CardItem>
+                                <CardItem bordered>
+                                    <Text style={[styles.text]}>
+                                        {item.apellidos} , {item.nombres}
+                                    </Text>
+                                </CardItem>
                             </Card>
                         )}
                         keyExtractor={(item, index) => index.toString()}
@@ -64,6 +135,6 @@ export default class UserTrips extends React.Component {
 
 const styles = StyleSheet.create({
     text: {
-        color: "black"
+        fontFamily: 'NeoSans'
     },
 });
